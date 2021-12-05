e1d76e4278a91230eb2fafd42ccbaf957b3d477f
amitp <amitp@DESKTOP-0SP81J5.fios-router.home>
2021-12-05 12:05:05 -0500

    huffman

---
+++ src/com/classic/algorithms/HuffmanEncoding.java
@@ -0,0 +1,71 @@
+package com.classic.algorithms;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Node {
+
+	int data;
+	char c;
+
+	Node left;
+	Node right;
+}
+
+class TreeComparator implements Comparator<Node> {
+	public int compare(Node x, Node y) {
+
+		return x.data - y.data;
+	}
+}
+
+public class HuffmanEncoding {
+
+	public static void printHuffManTree(Node root, String s) {
+
+		if (root.left == null && root.right == null && Character.isLetter(root.c)) {
+
+			System.out.println(root.c + "->" + s);
+
+			return;
+		}
+
+		printHuffManTree(root.left, s + "0");
+		printHuffManTree(root.right, s + "1");
+	}
+
+	// main function
+	public static void main(String[] args) {
+
+		int n = 6;
+		char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f' };
+		int[] freqs = { 4, 4, 12, 13, 16, 44 };
+
+		PriorityQueue<Node> q = new PriorityQueue<Node>(n, new TreeComparator());
+
+		for (int i = 0; i < n; i++) {
+			Node currNode = new Node();
+			currNode.c = chars[i];
+			currNode.data = freqs[i];
+			currNode.left = null;
+			currNode.right = null;
+			q.add(currNode);
+		}
+
+		Node root = null;
+
+		while (q.size() > 1) {
+			Node x = q.poll();
+			Node y = q.poll();
+			Node f = new Node();
+			f.data = x.data + y.data;
+			f.c = '-';
+			f.left = x;
+			f.right = y;
+			root = f;
+			q.add(f);
+		}
+
+		printHuffManTree(root, "");
+	}
+}
---
+++ src/com/classic/problems/Permutaion.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.classic.problems;
+
+/**
+ * @author amitp
+ *
+ */
+public class Permutaion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		permutation("Amit");
+
+	}
+
+	private static void permutation(String string) {
+		permutation("", "Amit");
+
+	}
+
+	private static void permutation(String prefix, String suffix) {
+		int length = suffix.length();
+		if (length == 0) {
+			System.out.println(prefix);
+		} else {
+			for (int i = 0; i < length; i++) {
+				permutation(prefix + suffix.charAt(i), suffix.substring(0, i) + suffix.substring(i + 1, length));
+
+			}
+		}
+
+	}
+
+}
